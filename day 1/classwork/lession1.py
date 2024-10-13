e643e738ab9bb2fa2f2d0f67dbd0ff21796590da
kopadzegvanca <kopadzegvanca880@gmail.com>
2024-10-13 16:39:30 +0400

    second commit

---
+++ day 1/classwork/lession1.py
@@ -0,0 +1,31 @@
+name="gvanca" # ეს არის str(string) ტიპის ცვლადი
+
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "nika" არის ცვლადის მნიშვნელობა
+
+surname="kopadze"
+
+print(name)
+# პრინტ ფინქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+age=22 # ეს არის int(integer) მთელი რიცხვი
+height=156.5 # ეს არის float ტიპის ცვლადი (ათწილადი)
+# boolean (bool)ტიპის ცვლადი
+
+knows_programing = True # True ან False
+is_ugly = False # ნებისმიერი ცვლადი რომელიც რამდენიმე სიტყისგან შედგება ქვედა ტირეთი (_) ვანცალკევებთ 
+#(snakecase – წერის სტილი)
+isUgly = False # ჯავასკრიპტული comeLcase
+
+#print(name + " " + surname) 
+#სტრინგი არის ბრჭყალებშ მოცემული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programing))
+
+print(name + " " + str(age))
---
+++ day 1/homework/homework1.py
@@ -0,0 +1,6 @@
+name = "gvanca"
+surname = "kopadze"
+age = 16
+height = 165.5
+
+print("me var " + name + " " +surname + ". var " + str(age) + " wlis " + "da " + str(height)+ " simaglis.")
