e9c1c5948087ca5f9a5700a28328f914df2ad438
kopadzegvanca <kopadzegvanca880@gmail.com>
2024-12-09 03:14:37 +0400

    nintheenth commit

---
+++ Day 18/Classwork/cw1.py
@@ -0,0 +1,21 @@
+# loop -ერთიდაიგივე რაღაცის მრავალჯერ გამეორება
+# ციკლი
+# range - ამ ფუნქციის დახმარებიტ ციკლს ვანიწებთ რეინჯს ანუ რამდენჯერ დატრიალდება (საზღვარი)
+
+
+# for loop
+# while loop
+
+# range ()
+# საიტერაციო ცვლადი (i - iteration)
+
+for i in range (10):
+    print ("hello")
+
+
+for i in range (99):
+    print(str(i) + "ily")
+
+
+for i in range(5):
+    print (i + 5) 
