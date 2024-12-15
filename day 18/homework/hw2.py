8a87a31091d369b849557272c799d13e2b905dfb
kopadzegvanca <kopadzegvanca880@gmail.com>
2024-12-15 12:45:31 +0400

    commit

---
+++ day 18/homework/hw2.py
@@ -0,0 +1,8 @@
+#2) მომხმარებელს შემოატანინეთ რიცხვი შემდეგ კი შეამოწმეთ ეს რიცხვი არის თუ არა ხუთის ჯერადი
+
+num = int(input("Enter Number: "))
+
+if num % 5 == 0:
+    print("aris 5 is jeradi")
+else:
+    print("ar aris 5 is jeradi")    
