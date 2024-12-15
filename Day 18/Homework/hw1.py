bf48525df3ce52f33b88bcec6616c5248bc85af8
kopadzegvanca <kopadzegvanca880@gmail.com>
2024-12-15 12:43:42 +0400

    commit

--- Day 18/Homework/hw1.py
+++
@@ -1,9 +0,0 @@
-# 1)   მომხმარებელს შემოატანინეთ რიცხვი შემდეგ კი შეამოწმეთ ეს რიცხვი არის თუ არა სამის ჯერადი
-
-
-num = int(input("Enter Number: "))
-
-if num % 3 == 0:
-    print("aris 3is jeradi")
-else:
-    print("ar aris 3is jeradi")    
